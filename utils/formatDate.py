37115a2061dbed7cff0a771e8f651ddf09595b04
SebaschaM <schaquila@autonoma.edu.pe>
2025-09-07 13:03:50 -0500

    formate date

---
+++ utils/formatDate.py
@@ -0,0 +1,15 @@
+# formatDate.py
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+def format_to_utc_minus5(iso_date: str) -> str:
+    # Parsear la fecha ISO (ej: 2025-09-07T18:00:53Z)
+    dt_utc = datetime.strptime(iso_date, "%Y-%m-%dT%H:%M:%SZ")
+    dt_utc = dt_utc.replace(tzinfo=ZoneInfo("UTC"))
+
+    # Convertir a UTC-5 (ej: America/Lima)
+    dt_local = dt_utc.astimezone(ZoneInfo("America/Lima"))
+
+    # Formato: dd-mm-aaaa hh-mm
+    return dt_local.strftime("%d-%m-%Y %H:%M")
