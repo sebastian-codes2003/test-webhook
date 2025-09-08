4020b44c84196a453bbb3ba5787a67a5009474b2
SebaschaM <schaquila@autonoma.edu.pe>
2025-09-07 22:42:49 -0500

    fix format_date.py2

--- utils/format_date.py
+++ utils/format_date.py
@@ -1,11 +1,20 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
 
 def formate_date(iso_date: str) -> str:
-    dt_utc = datetime.strptime(iso_date, "%Y-%m-%dT%H:%M:%SZ")
-    dt_utc = dt_utc.replace(tzinfo=ZoneInfo("Etc/UTC"))  # 👈 aquí el fix
 
+    # Intentar parseo con y sin milisegundos
+    try:
+        dt_utc = datetime.strptime(iso_date, "%Y-%m-%dT%H:%M:%SZ")
+    except ValueError:
+        dt_utc = datetime.strptime(iso_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    # Asignar timezone UTC (siempre disponible)
+    dt_utc = dt_utc.replace(tzinfo=timezone.utc)
+
+    # Convertir a tu zona local
     dt_local = dt_utc.astimezone(ZoneInfo("America/Lima"))
 
+    # Formatear
     return dt_local.strftime("%d-%m-%Y %H:%M")
